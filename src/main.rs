038e78af5966b156caac597ec140b4d9e17e049f
chris <chris@hargreaves.io>
2026-09-23 14:27:25 -0700

    move server helpers from main to server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::process::Command;
 use std::path::{Path, PathBuf};
 use std::error::Error;
 
@@ -29,49 +28,13 @@ struct LaunchArgs {
     configdir: PathBuf,
 }
 
-// convert unix path to Z:\ structure
-trait WinePath {
-    fn to_wine_path(&self) -> Result<String, Box<dyn Error>> ;
-}
-
-impl WinePath for Path {
-    fn to_wine_path(&self) -> Result<String, Box<dyn Error>> {
-        let abspath = self.canonicalize()
-            .map_err(|e| format!("{path}: {e}", path = self.to_string_lossy()))?
-            .display()
-            .to_string();
-
-        Ok(format!("Z:{}", abspath.replace("/", "\\")))
-    }
-}
-
-// generate server command in `serverdir` using config in `configdir`
-fn server_command(serverdir: String, configdir: &Path) -> Result<Command, Box<dyn Error>> {
-    let mut cmd = Command::new("wine");
-
-    let configjson = configdir.join("settings.json").to_wine_path()?;
-    let seasonjson = configdir.join("season.json").to_wine_path()?;
-
-    cmd.current_dir(serverdir)
-        .arg("AssettoCorsaEVOServer.exe")
-        .arg("-configjson")
-        .arg(configjson)
-        .arg("-seasonjson")
-        .arg(seasonjson)
-        .arg("-no_lobby");
-
-    Ok(cmd)
-}
-
-
-
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = CLI::parse();
     let serverdir = cli.serverdir.unwrap_or(".".to_string());
     match cli.command {
         Commands::Launch(args) => {
             let configpath = Path::new(&args.configdir);
-            let mut server = Supervisor::new(server_command(serverdir, &configpath)?);
+            let mut server = Supervisor::new(server::server_command(serverdir, &configpath)?);
             return server.run();
         }
     }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,40 @@
 use std::process::Command;
 use std::error::Error;
+use std::path::Path;
+
+// convert unix path to Z:\ structure
+trait WinePath {
+    fn to_wine_path(&self) -> Result<String, Box<dyn Error>> ;
+}
+
+impl WinePath for Path {
+    fn to_wine_path(&self) -> Result<String, Box<dyn Error>> {
+        let abspath = self.canonicalize()
+            .map_err(|e| format!("{path}: {e}", path = self.to_string_lossy()))?
+            .display()
+            .to_string();
+
+        Ok(format!("Z:{}", abspath.replace("/", "\\")))
+    }
+}
+
+// generate server command in `serverdir` using config in `configdir`
+pub fn server_command(serverdir: String, configdir: &Path) -> Result<Command, Box<dyn Error>> {
+    let mut cmd = Command::new("wine");
+
+    let configjson = configdir.join("settings.json").to_wine_path()?;
+    let seasonjson = configdir.join("season.json").to_wine_path()?;
+
+    cmd.current_dir(serverdir)
+        .arg("AssettoCorsaEVOServer.exe")
+        .arg("-configjson")
+        .arg(configjson)
+        .arg("-seasonjson")
+        .arg(seasonjson)
+        .arg("-no_lobby");
+
+    Ok(cmd)
+}
 
 pub struct Supervisor {
     command: Command, 
