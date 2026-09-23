6b814ea8b43b400b1e21aacb5f9fe0a90b19c97d
chris <chris@hargreaves.io>
2026-09-23 09:55:19 -0700

    print command and args being executed by supervisor

--- src/supervisor.rs
+++ src/supervisor.rs
@@ -12,6 +12,15 @@ impl Supervisor {
     }
 
     pub fn run(&mut self) {
+        let args: String = self.command.get_args()
+            .map(|x| x.to_string_lossy())
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        let prog = self.command.get_program().to_string_lossy();
+
+        println!("Exectuing: {} {}", prog, args);
+
         let cmd = self.command.spawn();
 
         let mut child = match cmd {
